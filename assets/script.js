f88593dc7bacf9950dfdcebcb0aa7fe88cfa5266
Yen Nguyen <112992331+yhnguyen720@users.noreply.github.com>
2023-01-02 00:22:00 -0600

    Create script.js

---
+++ assets/script.js
@@ -0,0 +1,41 @@
+$(function () {
+  
+    //using dayjs to print current date and define current hour
+    var today = dayjs();
+    $("#currentDay").text(today.format("dddd, MMMM D"));
+    var currentHour = dayjs().hour();
+  
+    //defining current hour by traversing through time block and splitting each hour-x id 
+    $(".time-block").each(function () {
+      var blockHour = parseInt($(this).attr("id").split("hour-")[1]);
+      //if else function for color block changes
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      }
+      else if (blockHour === currentHour) {
+        $(this).addClass("present");
+      }
+      else {
+        $(this).addClass("future");
+      }
+    });
+  
+    //function to local storage text upon button click
+    $(".saveBtn").on("click", function(){
+      var text = $(this).siblings(".description").val();
+      var time = $(this).parent().attr("id");
+      localStorage.setItem(time, text);
+    });
+  
+    //saves and loads events 
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+  
+  });
